3e5a768147193820885b78a1ce19afc934d1f8c2
Jeffrey <jephroz@gmail.com>
2016-06-30 00:05:18 -0700

    add complete filter

--- server.js
+++ server.js
@@ -9,8 +9,22 @@ var todoNextId = 1;
 
 app.use(bodyParser.json())
 
+app.get('/', function (req, res) {
+	res.send('Todo API Root')
+})
+
 app.get('/todos', function (req, res) {
-	res.json(todos);
+	var queryParams = req.query;
+	var filteredTodos = todos;
+
+	//URL query
+	if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'true') {
+		filteredTodos = _.where(filteredTodos, {completed: true});
+	} else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
+		filteredTodos = _.where(filteredTodos, {completed: false})
+	}
+
+	res.json(filteredTodos);
 })
 
 app.get('/todos/:id', function (req, res) {
